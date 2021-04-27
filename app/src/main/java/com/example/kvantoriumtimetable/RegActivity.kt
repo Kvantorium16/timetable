c8d034db96f6ce1d8d9682ec099b3bb22dfc0cbb
Naturkoj <naturkoj21@mail.ru>
2021-04-27 18:19:08 +0300

    Переделал окно регистрации

--- app/src/main/java/com/example/kvantoriumtimetable/RegActivity.kt
+++ app/src/main/java/com/example/kvantoriumtimetable/RegActivity.kt
@@ -10,5 +10,7 @@ class RegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
+
+
     }
 }
